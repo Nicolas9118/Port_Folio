fca10e045a558fea3e167092bf8650e39b464371
Nicolas9118 <rolland.nicolas9118@outlook.com>
2022-12-20 11:34:21 +0100

    correction responsive

--- front/src/sections/Me/SectionAbout.jsx
+++ front/src/sections/Me/SectionAbout.jsx
@@ -14,9 +14,11 @@ const SectionAbout = () => {
             <h1> A Propos de moi </h1>
             <div className="container">
                 <div className="left">
-                    <p className="presentation"> Mon parcours professionnel ne démarre pas en tant que développeur web, cela m'a permis d'acquérir des compétences qui me permettes une bonne adaptabilités dans toutes sorte de situation.
-                        Diplomé tout de même d'un BTS Informatique en 2012 et une foramtion de développeur web en 2022 font de moi un développeur polyvalent via différent projet.
-                        Méticuleux, organisé et curieux j'aime travaillé sur plein de projets différents et de les peaufiner jusqu'au petits détails </p>
+                    <p className="presentation"> Mon parcours professionnel ne démarre pas en tant que développeur web. Mes expériences précédantes m'ont
+                        permis d'acquérir des compétences qui me permettent une bonne adaptabilité dans toute sorte de situation. Mon dipôme 
+                         BTS 
+                        informatique de 2012 et ma foramtion de développeur web en 2022 font de moi un développeur polyvalent à travers différents projets.
+                        Méticuleux, organisé et curieux, j'aime travailler divers projets et les peaufiner jusqu'aux moindre petits détails. </p>
                     <img src={MaxiSoulignement} alt="" />
                     <Bouton buttonName="Télécharger CV" /> 
                 </div>
